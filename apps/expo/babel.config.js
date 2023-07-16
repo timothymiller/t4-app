e6d725eeefff925d3898539299141ad24fc39c2a
Cody Gagliardo <Cody.Gagliardo12@gmail.com>
2023-07-16 21:22:54 +0000

    Chore - standard --fix repo

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -11,8 +11,8 @@ module.exports = function (api) {
           path: '../../.env',
           allowlist: ['NEXT_PUBLIC_SUPABASE_URL', 'NEXT_PUBLIC_SUPABASE_PUBLIC_KEY'],
           safe: false,
-          allowUndefined: true,
-        },
+          allowUndefined: true
+        }
       ],
       [
         require.resolve('babel-plugin-module-resolver'),
@@ -22,10 +22,10 @@ module.exports = function (api) {
             // define aliases to shorten the import paths
             app: '../../packages/app',
             '@t4/api': '../../packages/api',
-            '@t4/ui': '../../packages/ui',
+            '@t4/ui': '../../packages/ui'
           },
-          extensions: ['.js', '.jsx', '.tsx', '.ios.js', '.android.js'],
-        },
+          extensions: ['.js', '.jsx', '.tsx', '.ios.js', '.android.js']
+        }
       ],
       // if you want reanimated support
       // 'react-native-reanimated/plugin',
@@ -36,17 +36,17 @@ module.exports = function (api) {
               '@tamagui/babel-plugin',
               {
                 components: ['@t4/ui', 'tamagui'],
-                config: './tamagui.config.ts',
-              },
-            ],
+                config: './tamagui.config.ts'
+              }
+            ]
           ]),
       [
         'transform-inline-environment-variables',
         {
-          include: 'TAMAGUI_TARGET',
-        },
+          include: 'TAMAGUI_TARGET'
+        }
       ],
-      'jotai/babel/plugin-react-refresh',
-    ],
+      'jotai/babel/plugin-react-refresh'
+    ]
   }
 }
