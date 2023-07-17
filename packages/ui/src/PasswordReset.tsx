9d7fd1ea30a9dcff6e03c86b90227ed95c2daf0a
Death666Machine <Cody.Gagliardo12@gmail.com>
2023-07-16 18:25:34 -0700

    Fix - Update PasswordReset.tsx

--- packages/ui/src/PasswordReset.tsx
+++ packages/ui/src/PasswordReset.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import { YStack, Paragraph, Button, Input } from 'tamagui'
+import { Image, YStack, Paragraph, XStack, Button, Input, Stack } from 'tamagui'
+import { Link } from 'solito/link'
+import { type Provider } from '@supabase/supabase-js'
 
 interface Props {
-  type: 'email' | 'password'
-  handleWithPress: (emailOrPassword: string) => void
+  type: 'sign-up' | 'sign-in'
+  handleOAuthWithPress: (provider: Provider) => void
+  handleEmailWithPress: (emailAddress, password) => void
 }
 
-export const PasswordResetComponent: React.FC<Props> = ({ type, handleWithPress }) => {
-  const [emailOrPassword, setEmailOrPassword] = useState('')
+export const SignUpSignInComponent: React.FC<Props> = ({
+  type,
+  handleOAuthWithPress,
+  handleEmailWithPress,
+}) => {
+  const [emailAddress, setEmailAddress] = useState('')
+  const [password, setPassword] = useState('')
 
   return (
     <YStack
@@ -22,43 +30,137 @@ export const PasswordResetComponent: React.FC<Props> = ({ type, handleWithPress 
       bg="$background"
     >
       <Paragraph size="$5" fontWeight={'700'} opacity={0.8} mb="$1">
-        {type == 'email' ? 'Reset your password' : 'Change your password'}
+        {type === 'sign-up' ? 'Create your account' : 'Log in to your account'}
       </Paragraph>
+      {/* all the oauth sign up options */}
+      <XStack space jc={'space-evenly'} theme="light">
+        {/* 3 buttons, for google, apple, discord */}
+        <Button
+          size="$5"
+          onPress={() => handleOAuthWithPress('google')}
+          hoverStyle={{ opacity: 0.8 }}
+          focusStyle={{ scale: 0.95 }}
+          borderColor="$gray8Light"
+        >
+          <Image
+            style={{ width: 20, height: 20 }}
+            source={{ width: 20, height: 20, uri: 'auth/google-logo.png' }}
+            width="100%"
+            height="100%"
+            resizeMode="contain"
+            alt="Google Logo"
+          />
+        </Button>
+        <Button
+          size="$5"
+          onPress={() => handleOAuthWithPress('apple')}
+          hoverStyle={{ opacity: 0.8 }}
+          focusStyle={{ scale: 0.95 }}
+          borderColor="$gray8Light"
+        >
+          <Image
+            style={{ width: 22, height: 22 }}
+            source={{ width: 22, height: 22, uri: 'auth/apple-logo.png' }}
+            width="100%"
+            height="100%"
+            resizeMode="contain"
+            alt="Apple Logo"
+          />
+        </Button>
+        <Button
+          size="$5"
+          onPress={() => handleOAuthWithPress('discord')}
+          hoverStyle={{ opacity: 0.8 }}
+          focusStyle={{ scale: 0.95 }}
+          borderColor="$gray8Light"
+        >
+          <Image
+            style={{ width: 25, height: 22 }}
+            source={{ width: 25, height: 22, uri: 'auth/discord-logo.png' }}
+            width="100%"
+            height="100%"
+            resizeMode="contain"
+            alt="Discord Logo"
+          />
+        </Button>
+      </XStack>
+      <XStack ai="center" width="100%" jc="space-between">
+        <Stack h="$0.25" bg="black" w="$10" opacity={0.1} />
+        <Paragraph size="$3" opacity={0.5}>
+          or
+        </Paragraph>
+        <Stack h="$0.25" bg="black" w="$10" opacity={0.1} />
+      </XStack>
 
-      {/* email or password input */}
-      {type === 'email' ? (
-        <Input
-          autoCapitalize="none"
-          placeholder="Email"
-          onChangeText={(text) => {
-            setEmailOrPassword(text)
-          }}
-        />
-      ) : (
-        <Input
-          autoCapitalize="none"
-          placeholder="New Password"
-          onChangeText={(text) => {
-            setEmailOrPassword(text)
-          }}
-          textContentType="password"
-          secureTextEntry
-        />
-      )}
+      {/* email sign up option */}
+      <Input
+        autoCapitalize="none"
+        placeholder="Email"
+        onChangeText={(text) => {
+          setEmailAddress(text)
+        }}
+      />
+      <Input
+        autoCapitalize="none"
+        placeholder="Password"
+        onChangeText={(text) => {
+          setPassword(text)
+        }}
+        textContentType="password"
+        secureTextEntry
+      />
 
-      {/* reset password button */}
+      {/* sign up button */}
       <Button
         themeInverse
         onPress={() => {
-          handleWithPress(emailOrPassword)
+          handleEmailWithPress(emailAddress, password)
         }}
         hoverStyle={{ opacity: 0.8 }}
         onHoverIn={() => {}}
         onHoverOut={() => {}}
         focusStyle={{ scale: 0.975 }}
       >
-        {type == 'email' ? 'Reset Password' : 'Change Password'}
+        {type === 'sign-up' ? 'Sign up' : 'Sign in'}
       </Button>
+
+      {/* or sign in, in small and less opaque font */}
+      <XStack>
+        <Paragraph size="$2" mr="$2" opacity={0.4}>
+          {type === 'sign-up' ? 'Already have an account?' : 'Don’t have an account?'}
+        </Paragraph>
+        <Link href={type === 'sign-up' ? '/sign-in' : '/sign-up'}>
+          <Paragraph
+            cursor={'pointer'}
+            size="$2"
+            fontWeight={'700'}
+            opacity={0.5}
+            hoverStyle={{ opacity: 0.4 }}
+          >
+            {type === 'sign-up' ? 'Sign in' : 'Sign up'}
+          </Paragraph>
+        </Link>
+      </XStack>
+
+      {/* forgot password */}
+      {type === 'sign-in' && (
+        <XStack mt="$-2.5">
+          <Paragraph size="$2" mr="$2" opacity={0.4}>
+            Forgot your password?
+          </Paragraph>
+          <Link href="/password-reset">
+            <Paragraph
+              cursor={'pointer'}
+              size="$2"
+              fontWeight={'700'}
+              opacity={0.5}
+              hoverStyle={{ opacity: 0.4 }}
+            >
+              Reset it
+            </Paragraph>
+          </Link>
+        </XStack>
+      )}
     </YStack>
   )
 }
