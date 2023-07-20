02426f9bc7a12500bc00396bb0c99fdf9e38a2dc
Desttro <bozstvi-mluvil0a@icloud.com>
2023-07-20 10:51:59 +0200

    Expo 49

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,15 +1,19 @@
+import { ConfigContext, ExpoConfig } from '@expo/config'
+import dotenv from 'dotenv'
 
-import { ConfigContext, ExpoConfig } from '@expo/config';
-import dotenv from 'dotenv';
-
-dotenv.config();
+dotenv.config()
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   extra: {
     eas: {
-      projectId: process.env.EAS_PROJECT_ID ?? "timothymiller",
+      projectId: process.env.EAS_PROJECT_ID ?? 'timothymiller',
     },
   },
-  owner: process.env.EAS_OWNER ?? "85fc6ccd-0ce1-4e4d-804c-b15df989f97e",
-});
+  owner: process.env.EAS_OWNER ?? '85fc6ccd-0ce1-4e4d-804c-b15df989f97e',
+  plugins: ['expo-router'],
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+})
