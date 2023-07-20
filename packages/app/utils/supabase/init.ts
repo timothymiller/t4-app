17847ea3f8624b16f86f0a7dc9dabc07a04d4dfc
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-07-19 21:47:59 -0400

    Set persistSession to false in supabaseClient

--- packages/app/utils/supabase/init.ts
+++ packages/app/utils/supabase/init.ts
@@ -4,4 +4,8 @@ import 'react-native-url-polyfill/auto'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+  },
+})
