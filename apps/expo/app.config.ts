f57578b600fd1c54a184774c61c2dcbd5b8124c3
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-07-25 17:16:06 -0400

    Set tamagui target per platform in dev & CI/CD (was deleted in another PR) Bumped expo version Added expo tools to vs code extensions

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -7,10 +7,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   extra: {
     eas: {
-      projectId: process.env.EAS_PROJECT_ID ?? 'timothymiller',
+      projectId: process.env.EAS_PROJECT_ID || 'timothymiller',
     },
   },
-  owner: process.env.EAS_OWNER ?? '85fc6ccd-0ce1-4e4d-804c-b15df989f97e',
+  owner: process.env.EAS_OWNER || '85fc6ccd-0ce1-4e4d-804c-b15df989f97e',
   plugins: ['expo-router'],
   experiments: {
     tsconfigPaths: true,
