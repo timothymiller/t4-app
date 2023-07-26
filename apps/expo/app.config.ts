782a4abc294b008704150a06bdb55371c972989d
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-07-26 10:36:45 -0400

    Relaxed node & pnpm requirements for EAS build (Android)

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -7,10 +7,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   extra: {
     eas: {
-      projectId: process.env.EAS_PROJECT_ID || 'timothymiller',
+      projectId: process.env.EAS_PROJECT_ID || '85fc6ccd-0ce1-4e4d-804c-b15df989f97e',
     },
   },
-  owner: process.env.EAS_OWNER || '85fc6ccd-0ce1-4e4d-804c-b15df989f97e',
+  owner: process.env.EAS_OWNER || 'timothymiller',
   plugins: ['expo-router'],
   experiments: {
     tsconfigPaths: true,
