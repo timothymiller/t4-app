229d53b0475b6dc5b18e55e4365a0d5a69a99c55
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-02 16:37:22 -0400

    Bump tamagui version

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -83,5 +83,5 @@ module.exports = function () {
     }
   }
 
-  return config
+  return config;
 }
--- packages/app/utils/supabase/init.ts
+++ packages/app/utils/supabase/init.ts
@@ -3,7 +3,7 @@ import { ExpoSecureStoreAdapter } from './cache'
 import 'react-native-url-polyfill/auto'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
