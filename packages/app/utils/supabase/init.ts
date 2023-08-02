5d95c7a4da35130c661f1b51d896f5a3e8c09c00
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-01 22:03:01 -0400

    added react-native-async-storage for expo

--- packages/app/utils/supabase/init.ts
+++ packages/app/utils/supabase/init.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import 'react-native-url-polyfill/auto'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
@@ -6,6 +7,9 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    persistSession: false,
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
   },
 })
