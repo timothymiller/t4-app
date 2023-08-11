e4cfa33805124e300b2205602747708ccac6c1b3
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-11 02:29:35 -0400

    Improved documentation & bump dependency versions

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
 import { useConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const title = 'T4 App'
 
 const config: DocsThemeConfig = {
-  logo: <span>📚 {title} Docs</span>,
+  logo: (
+    <>
+      <Image src="/t4-logo.png" width={42} height={42} alt={`${title} logo`} />
+      <span style={{ paddingLeft: 8 }}>{title} Docs</span>
+    </>
+  ),
   project: {
     link: 'https://github.com/timothymiller/t4-app',
   },
@@ -15,7 +21,23 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/timothymiller/t4-app/blob/main/apps/docs',
   footer: {
-    text: '©️ Copyright 2023 Timothy Miller',
+    text: '©️ Copyright 2023 - T4 App Agency, LLC',
+  },
+  primaryHue: 295,
+  banner: {
+    key: 'book-a-call',
+    text: (
+      <a
+        href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ1A5PlGGerAUohHYZ2a6knxIcdoHh17FckhySU-w0V-eweV7ZFR7DK03o3sxnWfwoEr0_mNR80J"
+        target="_blank"
+      >
+        👨‍💼💼 Hire us for your next project. Book a call →
+      </a>
+    ),
+  },
+  sidebar: {
+    autoCollapse: false,
+    defaultMenuCollapseLevel: 100,
   },
   useNextSeoProps() {
     const { asPath } = useRouter()
