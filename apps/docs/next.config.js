92a6c944ea62802376a339cae8baede0aa166d13
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-12 18:35:54 -0400

    PWA support for T4 App Docs

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,4 +3,9 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx'
 })
 
-module.exports = withNextra()
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA(withNextra())
