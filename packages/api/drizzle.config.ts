f31f8d066ec2dc9c69e351995ca1c4628a80b2de
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-22 00:49:53 -0400

    Fixed wrangler compatibility date

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from "drizzle-kit";
  
 export default {
+  driver: "better-sqlite",
+  dbCredentials: {
+    url: './db.sqlite',
+  },
   schema: "./src/db/schema.ts",
   out: "./migrations",
 } satisfies Config;
