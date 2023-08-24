6d25513e3f0197467c77946cdc48d89d9e70d14e
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2023-08-24 10:40:46 +0000

    Added basic account deletion page

---
+++ apps/next/pages/delete-account.tsx
@@ -0,0 +1,13 @@
+import { DeleteAccountScreen } from 'app/features/delete-account/screen'
+import Head from 'next/head'
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>Delete Account</title>
+      </Head>
+      <DeleteAccountScreen />
+    </>
+  )
+}
