6356dfa8b7ef72d70df063fea625ad0d223c9058
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-23 23:53:03 -0400

    Added D1 docs

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,10 +1,17 @@
-import type { Config } from "drizzle-kit";
- 
+import type { Config } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+dotenv.config({ path: './.dev.vars' })
+
+const DATABASE_ID = process.env.DATABASE_ID
+if (DATABASE_ID === undefined) {
+  throw new Error('DATABASE_ID is undefined')
+}
+
 export default {
-  driver: "better-sqlite",
+  driver: 'better-sqlite',
   dbCredentials: {
-    url: './db.sqlite',
+    url: './.wrangler/state/v3/d1/' + DATABASE_ID + '/db.sqlite',
   },
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
-} satisfies Config;
+  schema: './src/db/schema.ts',
+  out: './migrations',
+} satisfies Config
