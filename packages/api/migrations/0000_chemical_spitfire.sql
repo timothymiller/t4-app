9ec56f616f1dc75070962d87671aee4ee5911c16
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-08-24 03:28:53 -0400

    Fixed migrations

--- packages/api/migrations/0000_chemical_spitfire.sql
+++ packages/api/migrations/0000_chemical_spitfire.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `cars` (
+CREATE TABLE `Car` (
 	`id` text PRIMARY KEY NOT NULL,
 	`make` text NOT NULL,
 	`model` text NOT NULL,
@@ -8,4 +8,9 @@ CREATE TABLE `cars` (
 	`mileage` integer NOT NULL,
 	`fuelType` text NOT NULL,
 	`transmission` text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `User` (
+	`id` text PRIMARY KEY NOT NULL,
+	`email` text NOT NULL
 );
--- packages/api/migrations/0000_red_brood.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`email` text NOT NULL
-);
