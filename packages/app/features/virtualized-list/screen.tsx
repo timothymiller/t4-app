922ebe1eaf8c91f5f0e34ae3a29ad21aacc14bd5
Rob Marscher <rob@robmarscher.com>
2023-09-01 17:01:34 -0400

    Update virtualized list demo
    Connect to trpc.car.all query
    Update web implementation to use tamagui stack wrapper
    Update web implementation to work with onScroll
    Add back button header with animation height on scroll

--- packages/app/features/virtualized-list/screen.tsx
+++ packages/app/features/virtualized-list/screen.tsx
@@ -1,20 +1,62 @@
-import { Paragraph, VirtualList, YStack } from '@t4/ui'
+import type { Car } from '@t4/api/src/db/schema'
+import { Button, Paragraph, Spinner, VirtualList, YStack } from '@t4/ui'
+import { ArrowLeft } from '@tamagui/lucide-icons'
+import { formatNumber, formatPrice } from 'app/utils/number'
+import { trpc } from 'app/utils/trpc'
+import { useRef } from 'react'
 import { SolitoImage } from 'solito/image'
+import { useLink } from 'solito/link'
+import { Animated } from 'react-native'
+
+const MAX_HEADER_HEIGHT = 75
+const MIN_HEADER_HEIGHT = 40
+const SCROLL_RANGE = MAX_HEADER_HEIGHT - MIN_HEADER_HEIGHT
 
 export const VirtualizedListScreen = (): React.ReactNode => {
-  return <VirtualList data={data} renderItem={Item} itemHeight={80} />
+  const query = trpc.car.all.useQuery()
+  const backLink = useLink({
+    href: '/',
+  })
+  const scrollOffsetY = useRef(new Animated.Value(0)).current
+  const animatedHeaderHeight = scrollOffsetY.interpolate({
+    inputRange: [0, SCROLL_RANGE],
+    outputRange: [MAX_HEADER_HEIGHT , MIN_HEADER_HEIGHT],
+    extrapolate: 'clamp'
+  })
+
+  if (query.isInitialLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <YStack fullscreen flex={1}>
+      <Button {...backLink} animation={["quick", { height: { overshootClamping: true } }]} icon={ArrowLeft} chromeless backgroundColor="$background" br="$0" jc="flex-start" h={animatedHeaderHeight}>Back</Button>
+      {query.error ? (
+          <Paragraph>Error fetching cars: {query.error.message}</Paragraph>
+        ) : null}
+      {query.data?.length ? (
+        <VirtualList flex={1} data={query.data} renderItem={CarListItem} estimatedItemSize={80} pt="$3"
+          scrollEventThrottle={10}
+          onScroll={Animated.event(
+            [{ nativeEvent: {contentOffset: {y: scrollOffsetY}}}],
+            {useNativeDriver: false}
+          )}
+        />
+      ) : (
+        <>
+          {query.error ? (
+            <Paragraph>Error fetching cars: {query.error.message}</Paragraph>
+          ) : null}
+          {!query.isLoading && (
+            <Paragraph>No cars found.</Paragraph>
+          )}
+        </>
+      )}
+    </YStack>
+  )
 }
 
-interface Item {
-  title: string
-  index: number
-}
-
-const data: Item[] = Array(100)
-  .fill(0)
-  .map((_, v) => ({ title: `Item ${v + 1}`, index: v }))
-
-const Item = (item: Item): React.ReactElement => {
+const CarListItem = (car: Car): React.ReactElement => {
   return (
     <YStack flexDirection="row" paddingLeft="$2">
       <SolitoImage
@@ -28,10 +70,10 @@ const Item = (item: Item): React.ReactElement => {
       />
       <YStack>
         <Paragraph paddingTop="$2" paddingLeft="$3" paddingBottom="$1" fontSize={16}>
-          {'Item ' + item.index}
+          {car.make + ' ' + car.model}
         </Paragraph>
         <Paragraph paddingLeft="$3" fontSize={16} opacity={0.6}>
-          Subtitle
+          {car.color} - {car.year} - {formatNumber(car.mileage)} miles - {formatPrice(car.price)}
         </Paragraph>
       </YStack>
     </YStack>
--- packages/ui/src/list/index.web.tsx
+++ packages/ui/src/list/index.web.tsx
@@ -1,53 +1,92 @@
 import { useVirtualizer } from '@tanstack/react-virtual'
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
+import { Stack } from 'tamagui'
+import type { FlashListProps, ListRenderItemInfo } from '@shopify/flash-list'
 
-interface Props {
-  data: any[]
-  renderItem: (item: any) => React.ReactNode
-  itemHeight: number
-}
 
-export const VirtualList = ({ data, renderItem, itemHeight }: Props): React.ReactNode => {
-  const parentRef = useRef()
+export function VirtualList<T>(props: FlashListProps<T>): React.ReactNode {
+  // TODO there are more FlashListProps that should be omitted here...
+  const { data, estimatedItemSize, renderItem, onScroll, scrollEventThrottle, ...scrollableNodeProps } = props;
+  const parentRef = useRef<HTMLDivElement>(null)
   const rowVirtualizer = useVirtualizer({
-    count: data.length,
-    getScrollElement: () => parentRef.current as any,
-    estimateSize: () => itemHeight,
+    count: data?.length || 0,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => estimatedItemSize || 100,
   })
 
+  useEffect(() => {
+    console.log('total size', rowVirtualizer.getTotalSize())
+  }, [rowVirtualizer.getTotalSize()]);
+
   return (
-    <div
+    <Stack
       ref={parentRef as any}
-      style={{
-        height: `100%`,
-        overflow: 'auto', // Make it scroll!
+      overflow="scroll"
+      {...scrollableNodeProps}
+      onScroll={(e) => {
+        if (!e.target['getBoundingClientRect']) {
+          return;
+        }
+        const target = e.target as HTMLDivElement
+        const bb = target.getBoundingClientRect()
+        const scrollViewEvent = {
+          ...e,
+          currentTarget: target.scrollTop,
+          target: target.scrollTop,
+          nativeEvent: {
+            contentInset: {
+              left: 0,
+              top: 0,
+              bottom: 0,
+              right: 0,
+            },
+            contentOffset: {
+              x: target.scrollLeft,
+              y: target.scrollTop,
+            },
+            contentSize: {
+              width: target.scrollWidth,
+              height: target.scrollHeight,
+            },
+            layoutMeasurement: {
+              width: bb.width,
+              height: bb.height,
+            },
+            zoomScale: 1,
+          }
+        };
+        // console.log('onScroll', scrollViewEvent, onScroll);
+        onScroll?.(scrollViewEvent)
       }}
     >
-      {/* The large inner element to hold all of the items */}
-      <div
-        style={{
-          height: `${rowVirtualizer.getTotalSize()}px`,
-          width: '100%',
-          position: 'relative',
-        }}
-      >
-        {/* Only the visible items in the virtualizer, manually positioned to be in view */}
-        {rowVirtualizer.getVirtualItems().map((virtualItem) => (
-          <div
-            key={virtualItem.key}
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: `${virtualItem.size}px`,
-              transform: `translateY(${virtualItem.start}px)`,
-            }}
-          >
-            {renderItem(virtualItem)}
-          </div>
-        ))}
+      {/* Extra div so the inner element is affected by Stack flexbox */}
+      <div>
+        {/* The large inner element to hold all of the items */}
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {/* Only the visible items in the virtualizer, manually positioned to be in view */}
+          {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+            <div
+              key={virtualItem.key}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              }}
+            >
+              {renderItem?.(data![virtualItem.index] as ListRenderItemInfo<T>)}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </Stack>
   )
 }
