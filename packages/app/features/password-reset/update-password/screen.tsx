e92ce8cf8e1f5529a9aabb6b231f54908407a05d
Timothy Miller <46549361+timothymiller@users.noreply.github.com>
2023-09-18 03:12:57 -0400

    Re-add password reset functionality

--- packages/app/features/password-reset/update-password/screen.tsx
+++ packages/app/features/password-reset/update-password/screen.tsx
@@ -1,7 +1,6 @@
 import { YStack, useToastController } from '@t4/ui'
 import { useRouter } from 'solito/router'
 import { PasswordResetComponent } from '@t4/ui/src/PasswordReset'
-// import { updatePassword } from 'app/utils/supabase/auth'
 import { isExpoGo } from 'app/utils/flags'
 import { useSupabase } from 'app/utils/supabase/hooks/useSupabase'
 
@@ -10,23 +9,23 @@ export function UpdatePasswordScreen() {
   const toast = useToastController()
   const supabase = useSupabase()
 
-  const handlePasswordWithPress = async (password) => {
-    // const { error } = await supabase.auth.updateUser updatePassword(password)
-    // if (error) {
-    //   if (!isExpoGo) {
-    //     toast.show('Password change failed', {
-    //       description: error.message,
-    //     })
-    //   }
-    //   console.log('Password change failed', error)
-    //   return
-    // }
-    // push('/')
+  const handlePasswordUpdateWithPress = async (password) => {
+    const { error } = await supabase.auth.updateUser({ password })
+    if (error) {
+      if (!isExpoGo) {
+        toast.show('Password change failed', {
+          description: error.message,
+        })
+      }
+      console.log('Password change failed', error)
+      return
+    }
+    push('/')
   }
 
   return (
     <YStack flex={1} justifyContent="center" alignItems="center" space>
-      <PasswordResetComponent type="password" handleWithPress={handlePasswordWithPress} />
+      <PasswordResetComponent type="password" handleWithPress={handlePasswordUpdateWithPress} />
     </YStack>
   )
 }
