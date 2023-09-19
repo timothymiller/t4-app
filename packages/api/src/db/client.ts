55aa9e950c327d7c1c7fff11e36963791f25c72a
Timothy Miller <miller@genrae.com>
2023-09-19 18:35:19 -0400

    Attempt at bun:sqlite support in workerd

--- packages/api/src/db/client.ts
+++ packages/api/src/db/client.ts
@@ -1,3 +1,13 @@
 import { drizzle } from 'drizzle-orm/d1'
+// import { drizzle as drizzleBun, BunSQLiteDatabase } from 'drizzle-orm/bun-sqlite'
+// import { Database } from 'bun:sqlite'
 
-export const createDb = (d1: D1Database) => drizzle(d1)
+export const createDb = (d1: D1Database, isBunTime?: boolean, databaseId?: string) => {
+  // if (isBunTime === true) {
+  //   const pathSQLite = './.wrangler/state/v3/d1/' + databaseId + '/db.sqlite'
+  //   const sqlite = new Database(pathSQLite)
+  //   const db: BunSQLiteDatabase = drizzleBun(sqlite)
+  //   return db
+  // }
+  return drizzle(d1)
+}
