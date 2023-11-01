d31dfcdb726f00798a1dbbdb9252f79a12087b25
Timothy Miller <github@timknowsbest.com>
2023-11-01 16:53:19 -0400

    Added access to Drizzle Studio from CLI via print statement

--- packages/api/src/db/studio.ts
+++ packages/api/src/db/studio.ts
@@ -1,42 +1,41 @@
-import * as schema from "./schema";
-import studio, { Setup } from "@drizzle-team/studio";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
-import { readdir } from 'node:fs/promises';
-import { join } from 'node:path';
+import * as schema from './schema'
+import studio, { Setup } from '@drizzle-team/studio'
+import { drizzle } from 'drizzle-orm/bun-sqlite'
+import { Database } from 'bun:sqlite'
+import { readdir } from 'node:fs/promises'
+import { join } from 'node:path'
 
-async function getSqliteFile(){
+async function getSqliteFile() {
   try {
-      const path = "./state/v3/d1/miniflare-D1DatabaseObject/";
-      const fileNames = await readdir(path);
-      return fileNames.map(name => join(path, name)).filter(p => p.endsWith(".sqlite"))[0];
-  }
-  catch (err) {
-      console.error(err);
+    const path = './state/v3/d1/miniflare-D1DatabaseObject/'
+    const fileNames = await readdir(path)
+    return fileNames.map((name) => join(path, name)).filter((p) => p.endsWith('.sqlite'))[0]
+  } catch (err) {
+    console.error(err)
   }
 }
 
-const db = drizzle(new Database(await getSqliteFile()));
+const db = drizzle(new Database(await getSqliteFile()))
 const setup: Setup = {
-  type: "sqlite",
+  type: 'sqlite',
   db: db,
-  //@ts-expect-error
+  // @ts-expect-error
   schema: schema,
-};
+}
 
-//@ts-expect-error
-const server = await studio.prepareServer(setup);
+// @ts-expect-error
+const server = await studio.prepareServer(setup)
 
-const port = 1337;
-const host = "0.0.0.0";
+const port = 1337
+const host = '0.0.0.0'
 server.start({
   host,
   port,
   cb: (err: any, address: any) => {
     if (err) {
-      console.error(err);
+      console.error(err)
     } else {
       console.log(`Drizzle Studio running on ${address}`)
     }
   },
-});
+})
