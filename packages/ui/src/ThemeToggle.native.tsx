f8ebe2e0cbeeb23b4219140b7cf5058784f4db3a
Alberto Del Buono Paolini <alberto.delbuonopaolini@gmail.com>
2023-11-02 00:41:59 +0000

    Native theme toggle component

--- packages/ui/src/ThemeToggle.native.tsx
+++ packages/ui/src/ThemeToggle.native.tsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from "react";
+import { Monitor, Sun, Moon } from "@tamagui/lucide-icons";
 import type { ButtonProps } from "tamagui";
+import { Button } from "tamagui";
+import { ThemeVariant } from 'app/utils/theme'
+import { useAppTheme } from 'app/atoms/theme'
+import { useForceUpdate } from '@t4/ui'
 
+const icons = {
+  system: Monitor,
+  light: Sun,
+  dark: Moon
+}
 export const ThemeToggle = (props: ButtonProps) => {
-  // FIXME: implement a native ThemeToggle without using @tamagui/next-theme
-  return null;
+  const [clientTheme, setClientTheme] = useState<ThemeVariant>("system")
+  const [_, setAppTheme] = useAppTheme()
+  const forceUpdate = useForceUpdate()
+
+  useEffect(() => {
+    setAppTheme(clientTheme)
+    forceUpdate()
+  }, [clientTheme]);
+
+  return (
+    <Button
+      size="$4"
+      onPress={() => {
+        if(clientTheme === "system") {
+          setClientTheme("dark")
+        } else if(clientTheme === "dark") {
+          setClientTheme("light")
+        } else {
+          setClientTheme("system")
+        }
+      }}
+      aria-label="Toggle color scheme"
+      icon={icons[clientTheme]}
+      {...props}
+    />
+  );
 };
