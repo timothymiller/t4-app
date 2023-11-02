97dbcedb851ba9ff6559e6b9101050a2ca69da55
Alberto Del Buono Paolini <alberto.delbuonopaolini@gmail.com>
2023-11-02 17:01:15 +0000

    Readded check on NEXT_PUBLIC_APP_URL

--- apps/next/cfImageLoader.js
+++ apps/next/cfImageLoader.js
@@ -4,6 +4,10 @@ export default function cloudflareLoader ({ src, width, quality }) {
   // const params = [`width=${width}`, `quality=${quality || 75}`]
   const isDev = process.env.NODE_ENV === 'development'
   const appUrl = process.env.NEXT_PUBLIC_APP_URL
+
+  if (appUrl === undefined) {
+    throw new Error('NEXT_PUBLIC_APP_URL is undefined')
+  }
   
   if (isDev) {
     return `${appUrl}/${src}`
