762387ae18dd3ff8fe2b7572e2da98dde89424a1
Alberto Del Buono Paolini <alberto.delbuonopaolini@gmail.com>
2023-11-02 12:17:45 +0000

    Commented native theme toggle

--- packages/ui/src/ThemeToggle.native.tsx
+++ packages/ui/src/ThemeToggle.native.tsx
@@ -1,41 +1,41 @@
-import { useState, useEffect } from "react";
-import { Monitor, Sun, Moon } from "@tamagui/lucide-icons";
-import type { ButtonProps } from "tamagui";
-import { Button } from "tamagui";
-import { ThemeVariant } from 'app/utils/theme'
-import { useAppTheme } from 'app/atoms/theme'
-import { useForceUpdate } from '@t4/ui'
+// import { useState, useEffect } from "react";
+// import { Monitor, Sun, Moon } from "@tamagui/lucide-icons";
+// import type { ButtonProps } from "tamagui";
+// import { Button } from "tamagui";
+// import { ThemeVariant } from 'app/utils/theme'
+// import { useAppTheme } from 'app/atoms/theme'
+// import { useForceUpdate } from '@t4/ui'
 
-const icons = {
-  system: Monitor,
-  light: Sun,
-  dark: Moon
-}
-export const ThemeToggle = (props: ButtonProps) => {
-  const [clientTheme, setClientTheme] = useState<ThemeVariant>("system")
-  const [_, setAppTheme] = useAppTheme()
-  const forceUpdate = useForceUpdate()
+// const icons = {
+//   system: Monitor,
+//   light: Sun,
+//   dark: Moon
+// }
+// export const ThemeToggle = (props: ButtonProps) => {
+//   const [clientTheme, setClientTheme] = useState<ThemeVariant>("system")
+//   const [_, setAppTheme] = useAppTheme()
+//   const forceUpdate = useForceUpdate()
 
-  useEffect(() => {
-    setAppTheme(clientTheme)
-    forceUpdate()
-  }, [clientTheme]);
+//   useEffect(() => {
+//     setAppTheme(clientTheme)
+//     forceUpdate()
+//   }, [clientTheme]);
 
-  return (
-    <Button
-      size="$4"
-      onPress={() => {
-        if(clientTheme === "system") {
-          setClientTheme("dark")
-        } else if(clientTheme === "dark") {
-          setClientTheme("light")
-        } else {
-          setClientTheme("system")
-        }
-      }}
-      aria-label="Toggle color scheme"
-      icon={icons[clientTheme]}
-      {...props}
-    />
-  );
-};
+//   return (
+//     <Button
+//       size="$4"
+//       onPress={() => {
+//         if(clientTheme === "system") {
+//           setClientTheme("dark")
+//         } else if(clientTheme === "dark") {
+//           setClientTheme("light")
+//         } else {
+//           setClientTheme("system")
+//         }
+//       }}
+//       aria-label="Toggle color scheme"
+//       icon={icons[clientTheme]}
+//       {...props}
+//     />
+//   );
+// };
