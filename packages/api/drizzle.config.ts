72b733cc6f5a8a68f19a795ab3db23a498ef89f3
Timothy Miller <github@timknowsbest.com>
2023-11-09 20:50:43 -0500

    Official Drizzle Studio for D1 support

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,17 +1,13 @@
-import * as dotenv from 'dotenv'
 import type { Config } from 'drizzle-kit'
-dotenv.config({ path: './.dev.vars' })
-
-const DATABASE_ID = process.env.DATABASE_ID
-if (DATABASE_ID === undefined) {
-  throw new Error('DATABASE_ID is undefined')
-}
 
 export default {
-  driver: 'better-sqlite',
-  dbCredentials: {
-    url: `./.wrangler/state/v3/d1/${DATABASE_ID}/db.sqlite`,
-  },
   schema: './src/db/schema.ts',
   out: './migrations',
+  driver: 'd1',
+  dbCredentials: {
+    wranglerConfigPath: 'wrangler.toml',
+    dbName: 'production',
+  },
+  verbose: false,
+  strict: true,
 } satisfies Config
