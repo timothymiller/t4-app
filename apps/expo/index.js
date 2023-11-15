1dba9f3d31107d37b2ebc9ed3534232cee702d42
Rob Marscher <rob@robmarscher.com>
2023-11-15 08:45:01 -0500

    Add note about native URL coming in expo 50

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,5 +1,7 @@
-import 'react-native-url-polyfill/auto'
-import 'expo-router/entry'
-import { LogBox } from 'react-native'
-console.disableYellowBox = true
-LogBox.ignoreAllLogs()
+import "react-native-url-polyfill/auto";
+// ^^ Remove after upgrade to Expo v50
+// https://github.com/expo/expo/pull/24941
+import "expo-router/entry";
+import { LogBox } from "react-native";
+console.disableYellowBox = true;
+LogBox.ignoreAllLogs();
