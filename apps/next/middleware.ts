586dd6ef7878d2524268ba2d0b13f7fdd009df50
Rob Marscher <rob@robmarscher.com>
2023-12-28 16:34:54 -0500

    Apple oauth form handler needs edge runtime setting

--- apps/next/middleware.ts
+++ apps/next/middleware.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
+// Apple will POST form data to the redirect URI when scopes have been requested
+// This middleware enables getting the posted form data out of the request body
+// and sets it as JSON to a custom header which is then extracted in the
+// pages/oauth/[provider].tsx route component's getServerSideProps function
+// @link https://developer.apple.com/documentation/sign_in_with_apple/request_an_authorization_to_the_sign_in_with_apple_server
 export async function middleware(request: NextRequest) {
   if (request.method === 'POST' && request.body) {
     try {
