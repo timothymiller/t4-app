c0d46591f3f1c13464b564f8939d87a242f9fbb9
Rob Marscher <rob@robmarscher.com>
2024-01-05 17:48:40 -0500

    Slightly better ssr example

--- apps/next/app/ssr/page.tsx
+++ apps/next/app/ssr/page.tsx
@@ -3,5 +3,6 @@ import { SSRTestScreen } from './screen'
 export const runtime = 'edge'
 
 export default function Page() {
-  return <SSRTestScreen></SSRTestScreen>
+  const content = 'This page is rendered on the edge. It is not statically rendered.'
+  return <SSRTestScreen content={content}></SSRTestScreen>
 }
--- apps/next/app/ssr/screen.tsx
+++ apps/next/app/ssr/screen.tsx
@@ -2,11 +2,11 @@
 
 import { Paragraph, YStack } from '@t4/ui'
 
-export function SSRTestScreen() {
+export function SSRTestScreen(props: { content: string }) {
   return (
     <YStack flex={1}>
       <Paragraph role='heading'>
-        This page is rendered on the edge. It is not statically rendered.
+        {props.content}
       </Paragraph>
     </YStack>
   )
